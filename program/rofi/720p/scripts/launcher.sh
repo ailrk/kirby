def2e0559b200e60c33df502a6d2379bc14a1afb
Ailrk <jimmy123good@gmail.com>
2023-06-12 09:49:55 -0700

    make window mode the default rofi mode

--- program/rofi/720p/scripts/launcher.sh
+++ program/rofi/720p/scripts/launcher.sh
@@ -6,8 +6,9 @@
 ## Twitter : @adi1090x
 
 dir="$HOME/.config/rofi/theme/launcher/squared-material-red.rasi"
+modi=$1
 
-rofi -no-lazy-grab -show drun \
+rofi -no-lazy-grab -show "$modi" \
 -modi ,drun,window,ssh \
 -theme $dir/"$style" \
 -run-shell-command '{terminal} -e \\" {cmd}; read -n 1 -s"' \
