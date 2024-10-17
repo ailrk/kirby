97771c991a35104d610809afc5cf97b3be84a765
Ailrk <jimmy123good@gmail.com>
2024-10-17 00:46:24 -0700

    add fcitx for asahi linux

--- user/linux_m1/ailrk-asahi.nix
+++ user/linux_m1/ailrk-asahi.nix
@@ -182,6 +182,11 @@ with lib;
       pulseaudio = true;
     };
 
+    i18n.inputMethod = {
+      enabled = "fcitx5";
+      fcitx5.addons = with pkgs; with pkgs.fcitx5; [ fcitx5-rime fcitx5-mozc fcitx5-gtk fcitx5-chinese-addons ];
+    };
+
     fonts.fontconfig.enable = true;
 
     # User specific overlays.
