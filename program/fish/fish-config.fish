4db7aae50a7c386dd2686fe251ee5c9b9905471b
Ailrk <jimmy123good@gmail.com>
2024-11-09 17:28:53 -0800

    add cpwd

--- program/fish/fish-config.fish
+++ program/fish/fish-config.fish
@@ -20,4 +20,5 @@ alias fh="cat ~/.local/share/fish/fish_history"
 alias qq="exit"
 alias smli="rlwrap -A -p -r smlnj"
 alias ocamli="rlwrap -A -p -r ocaml"
+alias cpwd="pwd | xclip -sel clip"
 direnv hook fish | source
