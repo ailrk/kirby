6fe66c65d3bdf3c6f4dee4bf37e36477fd3c4ac5
Ailrk <jimmy123good@gmail.com>
2025-03-23 08:23:48 -0700

    Bump version

--- user/linux_m1/ailrk-asahi.nix
+++ user/linux_m1/ailrk-asahi.nix
@@ -113,7 +113,7 @@ with lib;
 
     i18n.inputMethod = {
       enabled = "fcitx5";
-      fcitx5.addons = with pkgs; with pkgs.fcitx5; [ fcitx5-rime fcitx5-mozc fcitx5-gtk fcitx5-chinese-addons ];
+      fcitx6.addons = with pkgs; with pkgs.fcitx5; [ fcitx5-rime fcitx5-mozc fcitx5-gtk fcitx5-chinese-addons ];
     };
 
     fonts.fontconfig.enable = true;
