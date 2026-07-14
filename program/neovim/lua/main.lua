e9ce4d9c68089ccb960f2f596f3df884fd8438fe
Ailrk <jimmy123good@gmail.com>
2026-07-13 22:36:20 -0700

    Update

--- program/neovim/lua/main.lua
+++ program/neovim/lua/main.lua
@@ -83,7 +83,7 @@ require('packer').startup(function(use)
     use {'mtth/scratch.vim'}
 
     use {
-      "/home/fatmonad/repo/markdown-preview.nvim",
+      vim.fn.expand("$HOME/repo/markdown-preview.nvim"),
       config = function()
         require("markdown_preview").setup({
           port = 8421,
